a86967211784c79d82556ca2fbb95d3455d644c6
AnonymousNovelite <anonymousnovelite@gmail.com>
2024-02-21 18:47:45 -0600

    added crt filter, added background

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import Chatbox from "./components/Chatbox";
+import autumnFeels from "./assets/autumn-feels.gif";
 import "./App.css";
 import "./index.css";
 
 function App() {
   return (
-    <>
+    <div id="root" className="backgroundImage aesthetic-effect-crt">
+      {/* <img src={autumnFeels} alt="" /> */}
       <Chatbox />
-      <div className="blocktest"></div>
-    </>
+      <div className="aesthetic-effect-crt"></div>
+    </div>
   );
 }
 
--- client/src/components/Chattext.jsx
+++ client/src/components/Chattext.jsx
@@ -1,7 +1,100 @@
 import React from "react";
-
+import { useState } from "react";
+const arrayText = [
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+  "text",
+];
+// const [dataSource, setDataSource] = useState([arrayText]);
 const Chattext = () => {
-  return <div className="chattext"></div>;
+  return (
+    <div className="chattext">
+      {arrayText.map((item, index) => {
+        return (
+          <div>
+            This is a div #{index + 1} {item}{" "}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Chattext;
