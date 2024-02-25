c907f0efde502ad01b09111686bfb4fcf444acde
AnonymousNovelite <anonymousnovelite@gmail.com>
2024-02-25 11:54:48 -0600

    made marquee animation

--- client/src/components/ChatHeader.jsx
+++ client/src/components/ChatHeader.jsx
@@ -2,22 +2,19 @@ import React from "react";
 import weNeedLogo from "../assets/welogo.png";
 
 const ChatHeader = () => {
-
-  };
   return (
     <div className="header">
-      <div className="logo" >
+      <div className="logo">
+        <h1 className="syncopate-regular gradient">
+          We Need To <span id="talk">Talk</span>
+        </h1>
         <img src={weNeedLogo} className="logo" alt="chat logo" />
-
-        <h1 className="syncopate-regular gradient">We Need To Talk</h1>
-        <div className="credits">
-          <h2 className="pixelify-sans gradient">
-            An Abisai Escobar and James Fern Production
-          </h2>
-          <h2 className="pixelify-sans gradient">
-            Backgrounds by aconfuseddragon
-          </h2>
-        </div>
+        <h2 className="pixelify-sans gradient" id="credit1">
+          An Abisai Escobar and James Fern Production
+        </h2>
+        <h2 className="pixelify-sans gradient" id="credit2">
+          Backgrounds by aconfuseddragon
+        </h2>
       </div>
     </div>
   );
